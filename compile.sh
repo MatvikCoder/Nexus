e782c83813451a67faf29b5eacaa00b3e8de69d6
MatvikCoder <zapasca567@gmail.com>
2026-03-01 22:51:49 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,16 @@
-nasm -f bin os.asm -o os.bin
-qemu-system-x86_64 os.bin
+#!/bin/bash
+
+# Компиляция загрузчика
+nasm -f bin boot.asm -o boot.bin
+
+# Компиляция ядра
+nasm -f bin kernel.asm -o kernel.bin
+
+# Объединение в один образ
+cat boot.bin kernel.bin > os.bin
+
+# Дополняем до размера дискеты (1.44 MB)
+truncate -s 1474560 os.bin
+
+# Запуск в QEMU
+qemu-system-x86_64 -drive format=raw,file=os.bin
