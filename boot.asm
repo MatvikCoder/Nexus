98c710fca895070e966e3e9d997f1435b88a87c1
MatvikCoder <zapasca567@gmail.com>
2026-03-07 23:03:57 +0300

    Починка загрузчика

--- boot.asm
+++ boot.asm
@@ -1,42 +1,78 @@
 [org 0x7c00]
 
 start:
+  ; Сохраняем номер загрузочного диска
+  mov [boot_drive], dl
+
   ; Очистка экрана
   mov ax, 0003h
   int 0x10
-  
+
   ; Вывод сообщения о загрузке
   mov si, loading_msg
   call print_string
-  
-  ; Загрузка ядра со второго сектора
-  mov ah, 0x02        ; Функция чтения диска
-  mov al, 0x14        ; Читаем 20 секторов (достаточно для ядра)
-  mov ch, 0x00        ; Цилиндр 0
-  mov cl, 0x02        ; Начинаем со второго сектора
-  mov dh, 0x00        ; Головка 0
-  mov dl, 0x80        ; Первый жесткий диск (для USB/флоппи используй 0x00)
-  mov bx, 0x1000      ; Сегмент для загрузки ядра
-  mov es, bx
-  xor bx, bx          ; Адрес 0x1000:0x0000
+
+  ; Сброс дисковой системы (на всякий случай)
+  mov ah, 0x00
+  mov dl, [boot_drive]
   int 0x13
-  
-  jc disk_error       ; Ошибка чтения
-  
-  ; Переход на загруженное ядро
+
+  ; Загрузка ядра с повторными попытками
+  mov si, 0x03        ; 3 попытки загрузки
+
+.retry:
+  push si
+
+  mov ah, 0x02        ; Функция чтения
+  mov al, 0x20        ; Читаем 32 сектора (16KB)
+  mov ch, 0x00        ; Цилиндр 0
+  mov cl, 0x02        ; Сектор 2
+  mov dh, 0x00        ; Головка 0
+  mov dl, [boot_drive] ; Используем сохраненный диск
+  mov bx, 0x1000      ; Сегмент
+  mov es, bx
+  xor bx, bx          ; es:bx = 0x1000:0x0000
+  int 0x13
+
+  pop si
+  jnc .success        ; Успешно?
+
+  ; Ошибка - уменьшаем счетчик попыток
+  dec si
+  jz disk_error       ; Попытки кончились
+
+  ; Сброс диска перед следующей попыткой
+  mov ah, 0x00
+  mov dl, [boot_drive]
+  int 0x13
+
+  ; Небольшая задержка (можно убрать)
+  mov cx, 0xFFFF
+.delay:
+  loop .delay
+
+  jmp .retry          ; Еще попытка
+
+.success:
   mov si, success_msg
   call print_string
-  
-  jmp 0x1000:0x0000   ; Прыгаем в ядро
+
+  ; Переход в защищенный режим (если нужно) или прямой прыжок
+  jmp 0x1000:0x0000
 
 disk_error:
   mov si, error_msg
   call print_string
+
+  ; Ждем нажатия клавиши
   mov ah, 0x00
-  int 0x16            ; Ждем нажатия клавиши
-  int 0x19            ; Перезагрузка
+  int 0x16
+
+  ; Перезагрузка через BIOS
+  int 0x19
 
 print_string:
+  pusha
   mov ah, 0x0e
 .loop:
   lodsb
@@ -45,14 +81,15 @@ print_string:
   int 0x10
   jmp .loop
 .done:
+  popa
   ret
 
-loading_msg db "Loading NovaNexus kernel...", 0x0D, 0x0A, 0
-success_msg db "Kernel loaded, starting...", 0x0D, 0x0A, 0
-error_msg db "Disk error! Press any key to reboot...", 0x0D, 0x0A, 0
+; Данные
+boot_drive  db 0
+loading_msg db "NovaNexus: Loading kernel...", 0x0D, 0x0A, 0
+success_msg db "NovaNexus: Kernel loaded, starting...", 0x0D, 0x0A, 0
+error_msg   db "NovaNexus: Disk error - system halted", 0x0D, 0x0A, 0
 
+; Заполнение до 510 байт и сигнатура
 times 510-($-$$) db 0
 dw 0xAA55
-
-; Заполняем оставшееся место до конца сектора нулями
-times 512-($-$$) db 0
